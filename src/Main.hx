aa486ada3d403d8a741e5ca705143bafec8c0545
Thanadev <thomas.lambert33720@gmail.com>
2017-06-23 14:28:20 +0200

    Init repository

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,22 @@
 package ;
-class Main {
-    public function new() {
+
+import com.thanadev.geneticsentence.views.MainView;
+import org.tamina.html.component.HTMLApplication;
+
+class Main extends HTMLApplication {
+
+    private static var _instance:Main;
+
+    private var _mainView:MainView;
+
+    public static function main() {
+        new Main();
+    }
+
+    private function new() {
+        super();
+
+        trace('loading comps');
+        loadComponents();
     }
 }
---
+++ src/com/thanadev/geneticsentence/views/MainView.hx
@@ -0,0 +1,11 @@
+package com.thanadev.geneticsentence.views;
+
+
+import org.tamina.html.component.HTMLComponent;
+
+@view("com/thanadev/geneticsentence/views/MainView.html")
+class MainView extends HTMLComponent {
+    override public function createdCallback():Void {
+        super.createdCallback();
+    }
+}
